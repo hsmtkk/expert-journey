f3300c29d0e0232d514a78b39b87867564cffeb5
hsmtkk <hsmtkk@gmail.com>
2024-01-12 13:14:24 +0900

    get username and password from env var

--- app/utils/database.ts
+++ app/utils/database.ts
@@ -2,7 +2,10 @@ import mongoose from "mongoose"
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://hk0316:olzyWvr73DcZNgHM@cluster0.8dm3jqk.mongodb.net/?retryWrites=true&w=majority")
+        const mongoUser = process.env.MONGO_USER
+        const mongoPassword = process.env.MONGO_PASSWORD
+        const mongoURL = `mongodb+srv://${mongoUser}:${mongoPassword}@cluster0.8dm3jqk.mongodb.net/?retryWrites=true&w=majority`
+        await mongoose.connect(mongoURL)
         console.log("Success: connected to MongoDB")
     } catch (err) {
         console.log("Failure: could not connect to MongoDB")
