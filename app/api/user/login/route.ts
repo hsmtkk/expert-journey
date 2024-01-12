e72c5ea646ff76be9d268a691651be7fa45c220a
hsmtkk <hsmtkk@gmail.com>
2024-01-12 10:57:38 +0900

    return JWT token when login

--- app/api/user/login/route.ts
+++ app/api/user/login/route.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server"
 import { connectDB } from "@/app/utils/database"
 import { UserModel } from "@/app/utils/schemaModels"
+import { SignJWT } from "jose"
+
+const SECRET_KEY = "fcb72196-264f-4cd2-bf0b-a7411e833ef8"
 
 export async function POST(request: Request) {
     const reqBody = await request.json()
@@ -9,7 +12,10 @@ export async function POST(request: Request) {
         const savedUser = await UserModel.findOne({ email: reqBody.email })
         if (savedUser) {
             if (reqBody.password === savedUser.password) {
-                return NextResponse.json({ message: "ログイン成功" })
+                const payload = { email: reqBody.email }
+                const secretKey = new TextEncoder().encode(SECRET_KEY)
+                const token = await new SignJWT(payload).setProtectedHeader({ alg: "HS256" }).setExpirationTime("1d").sign(secretKey)
+                return NextResponse.json({ message: "ログイン成功", token })
             } else {
                 return NextResponse.json({ message: "ログイン失敗、パスワード誤り" })
             }
